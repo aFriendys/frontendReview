bb28f99156f9bbba4d01d70840ee8b616f39ecc9
aFriend <hsphsphsp@yandex.ru>
2023-02-17 15:20:38 +0300

    start

---
+++ src/widgets/card/constants.ts
@@ -0,0 +1,6 @@
+import { ICard } from "../../types/types";
+
+export const cardsData: ICard[] = [
+  { id: 1, title: "cardTitle", description: "cardDesctiption" },
+  { id: 2, title: "cardTitle", description: "cardDesctiption" },
+];
